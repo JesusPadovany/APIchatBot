fdbcc91dec37699fc59c8bda1614ab7ad45a66c3
Génova Castillo <37961016+GenovaC@users.noreply.github.com>
2019-05-06 15:35:54 -0400

    Add an environment for Django Rest Framework
    Se añade un ambiente que tiene instalado django rest framework.

---
+++ DjangoRestFrameworkEnvironment/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\xpectra\documents\respaldo personal genova\jesusgenovachatbot\apichatbot\djangorestframeworkenvironment\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
