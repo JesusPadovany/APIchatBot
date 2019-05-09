ce2f4455c4dc9db6c6a716afa6a56f19e1b0ab5a
Jesus Ruiz <jesuspadovany@gmail.com>
2019-05-09 18:56:54 -0400

    Imagenes

--- bender/admin.py
+++ bender/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Question, Answer, Reflection
+from .models import Question, Answer, Reflection, Product, ProductImage
 
 admin.site.register(Question)
 admin.site.register(Answer)
 admin.site.register(Reflection)
+admin.site.register(Product)
+admin.site.register(ProductImage)
